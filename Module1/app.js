adb0ee850176a7fc7dce60bd8542e62f23841f16
LamaMohsen <lama.mh217@gmail.com>
2024-01-14 22:50:31 +0300

    Assignment-1

--- Module1/app.js
+++ Module1/app.js
@@ -1,33 +1,45 @@
-(function () {
+(function() {
   'use strict';
+  angular.module('eatLunch', []).controller('eatingController', handler);
 
-  angular.module('LunchCheck', [])
-    .controller('LunchCheckController', LunchCheckController);
-
-    LunchCheckController.$inject = ['$scope'];
-
-  function LunchCheckController($scope) {
-    $scope.lunchItems = '';
-    $scope.resultMessage = '';
-
-    $scope.checkLunch = function () {
-      var lunchItemsArray = $scope.lunchItems.split(',');
-      var itemCount = lunchItemsArray.length;
-
-      // Remove any empty items from the count
-      lunchItemsArray.forEach(function (item) {
-        if (item.trim() === '') {
-          itemCount--;
-        }
-      });
-
-      if (itemCount === 0) {
-        $scope.resultMessage = 'Please enter data first';
-      } else if (itemCount <= 3) {
-        $scope.resultMessage = 'Enjoy!';
-      } else {
-        $scope.resultMessage = 'Too much!';
-      }
-    };
+  function handler($scope) {
+      $scope.foodsString = "";
+      $scope.foodsCount = 0;
+      $scope.msg = "";
+      $scope.foodsCalc = function() {
+          let foodsCount = 0
+          if ($scope.foodsString == "") {
+              foodsCount = 0;
+          }
+          else {
+              let foods = $scope.foodsString.split(",");
+          for (let j = 0; j < foods.length; j++) {
+              let foodV2 = foods[j].split(' ');
+              for (let i = 0;i < foodV2.length; i++) {
+                  if (foodV2[i] == ''){
+                      foodV2.splice(i,1);
+                      i = i-1;
+                  }
+              }
+              if (foodV2.length == 0) {
+                  foods.splice(j,1);
+              }
+          }
+          console.log(foods);
+          foodsCount = foods.length;
+          }
+          $scope.msg = message(foodsCount);
+      };
+      let message = function(count) {
+          if (count > 3) {
+              return "Too much!";
+          }
+          else if (count == 0) {
+              return "Please enter data first";
+          }
+          else {
+              return "Enjoy!";
+          }
+      };
   }
-})();
+})()
